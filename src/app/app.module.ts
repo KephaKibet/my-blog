6a6d7f5785fcc48cb7eb72a559aad5db8e164dd0
Kepha Kibet <kibet.kepha@live.com>
2023-05-06 16:29:58 +0300

    install & configure redux dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,RouterModule,AuthModule
+    BrowserModule,RouterModule,AuthModule, StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -1,5 +1,5 @@
 import { createAction, props } from '@ngrx/store';
-import { RegisterRequestInterface } from '../types/registerRequest';
+import { RegisterRequestInterface } from '../types/registerRequest.interface';
 import { ActionTypes } from './actionTypes';
 
 export const registerAction = createAction(
